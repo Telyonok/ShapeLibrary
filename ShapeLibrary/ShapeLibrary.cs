63f2471a96f3146c7b8ca9a4902c3466b4b51a21
Telyonok <Life270304@mail.ru>
2023-04-21 19:28:10 +0300

    Initial commit

---
+++ ShapeLibrary/ShapeLibrary.cs
@@ -0,0 +1,53 @@
+﻿namespace ShapeLibrary
+{
+    public interface IShape
+    {
+        public double GetArea();
+    }
+
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentOutOfRangeException("Radius can not be less or equal to zero.");
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentOutOfRangeException("Sides can not be less or equal to zero.");
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double GetArea()
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+        public bool IsRightAngled()
+        {
+            return a * a + b * b == c * c 
+                || b * b + c * c == a * a 
+                || a * a + c * c == b * b;
+        }
+    }
+}
---
+++ ShapeLibraryTests/ShapeLibraryTest.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using ShapeLibrary;
+
+namespace ShapeLibraryTests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+
+        [TestCase(1, 1, 1, 0.4330)]
+        [TestCase(3, 4, 5, 6)]
+        [TestCase(5, 5, 5, 10.8253)]
+        public void GetArea_ReturnsCorrectValue(double a, double b, double c, double expectedArea)
+        {
+            var triangle = new Triangle(a, b, c);
+
+            var area = triangle.GetArea();
+
+            Assert.That(area, Is.EqualTo(expectedArea).Within(0.0001));
+        }
+
+        [Test]
+        public void GetArea_AllZeroes_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(0, 0, 0));
+        }
+
+        [Test]
+        public void GetArea_OneZero_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(0, 1, 1));
+        }
+
+        [Test]
+        public void GetArea_NegativeValue_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(-1, 1, 1));
+        }
+
+        [Test]
+        public void IsRightAngled_3_4_5_ReturnsTrue()
+        {
+            var triangle = new Triangle(3, 4, 5);
+
+            Assert.True(triangle.IsRightAngled());
+        }
+
+        [Test]
+        public void IsRightAngled_4_4_4_ReturnsFalse()
+        {
+            var triangle = new Triangle(4, 4, 4);
+
+            Assert.False(triangle.IsRightAngled());
+        }
+    }
+
+    [TestFixture]
+    public class CircleTests
+    {
+        [TestCase(1, Math.PI)]
+        [TestCase(2, 4 * Math.PI)]
+        [TestCase(3, 9 * Math.PI)]
+        public void GetArea_ValidValues_ReturnsCorrectValue(double radius, double expectedArea)
+        {
+            var circle = new Circle(radius);
+
+            var area = circle.GetArea();
+
+            Assert.That(area, Is.EqualTo(expectedArea).Within(0.0001));
+        }
+
+        [Test]
+        public void GetArea_Zero_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(0));
+        }
+
+        [Test]
+        public void GetArea_NegativeValue_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(-3));
+        }
+    }
+}
